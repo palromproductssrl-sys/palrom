f73a6f01c9422bb7ffeb609cade06348af01099f
radder1972 <matthias.radder@gmail.com>
2026-06-21 12:47:33 +0200

    performance: enable experimental inlineCss to resolve render-blocking stylesheet warnings

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['pdfkit'],
+  experimental: {
+    inlineCss: true,
+  },
 };
 
 export default nextConfig;
